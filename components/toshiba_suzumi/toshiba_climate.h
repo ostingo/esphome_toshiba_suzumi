976454287c2584b7c37929be54b2819bf7ed49eb
Ostingo <168398917+michelostojski@users.noreply.github.com>
2025-07-24 14:54:12 +0200

    Update toshiba_climate.h
    fan speed lower

--- components/toshiba_suzumi/toshiba_climate.h
+++ components/toshiba_suzumi/toshiba_climate.h
@@ -8,15 +8,11 @@
 #include "toshiba_climate_mode.h"
 #include "esphome/core/component.h"
 
-
 namespace esphome {
 namespace toshiba_suzumi {
 
-
 static const char *const TAG = "ToshibaClimateUart";
-// default max temp for units
 static const uint8_t MAX_TEMP = 30;
-// default min temp for units without 8° heating mode
 static const uint8_t MIN_TEMP_STANDARD = 17;
 static const uint8_t SPECIAL_TEMP_OFFSET = 16;
 static const uint8_t SPECIAL_MODE_EIGHT_DEG_MIN_TEMP = 5;
@@ -42,7 +38,6 @@ struct ToshibaCommand {
 
 class ToshibaClimateUart : public PollingComponent, public climate::Climate, public uart::UARTDevice {
  public:
-  public:
   void set_external_sensor(esphome::sensor::Sensor *sensor) { external_sensor_ = sensor; }
   void setup() override;
   void loop() override;
@@ -57,14 +52,16 @@ class ToshibaClimateUart : public PollingComponent, public climate::Climate, pub
   void disable_wifi_led(bool disabled) { wifi_led_disabled_ = disabled; }
   void set_special_mode_select(select::Select *special_mode_select) { special_mode_select_ = special_mode_select; }
   void set_min_temp(uint8_t min_temp) { min_temp_ = min_temp; }
+  void set_fan_speed_delay(uint32_t delay) { fan_speed_delay_ = delay; }  // <-- FAN SPEED DELAY SETTER
 
  protected:
-  /// Override control to change settings of the climate device.
   esphome::sensor::Sensor *external_sensor_{nullptr};
   void control(const climate::ClimateCall &call) override;
-
-  /// Return the traits of this controller.
   climate::ClimateTraits traits() override;
+
+  // --- FAN SPEED DELAY MEMBERS ---
+  uint32_t fan_speed_delay_ = 30;     // Delay in seconds, default 30
+  uint32_t reached_temp_time_ = 0;    // Timestamp when target temp reached
 
  private:
   std::vector<uint8_t> rx_message_;
@@ -76,7 +73,7 @@ class ToshibaClimateUart : public PollingComponent, public climate::Climate, pub
   select::Select *pwr_select_ = nullptr;
   sensor::Sensor *outdoor_temp_sensor_ = nullptr;
   bool horizontal_swing_ = false;
-  uint8_t min_temp_ = 17; // default min temp for units without 8° heating mode
+  uint8_t min_temp_ = 17;
   bool wifi_led_disabled_ = false;
   select::Select *special_mode_select_ = nullptr;
 
